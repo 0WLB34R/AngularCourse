36aafc49e4418038789a059e940af06e9e63118d
0WLB34R <knhrvs@gmail.com>
2021-08-05 18:21:35 -0400

    Operator Examples

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,72 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angCou';
+
+  constructor(){
+    const testMap = [1,2,3,4,5,6].map(item => item*2);
+    console.log(testMap);
+
+    const testforEach = [1,2,3,4,5,6].forEach(item => item*2);
+    console.log(testforEach);
+
+    const testFind = [1,2,6,4,5,6].find(item => item === 6);
+    console.log(testFind);
+
+    const testFilter = [1,2,3,4,5,6].filter(item => item % 2 === 0);
+    console.log(testFilter);
+
+    const testFindIndex = [1,2,2,4,5,6].findIndex(item => item === 2);
+    console.log(testFindIndex);
+
+    const testIndexOf = "Ken".indexOf("K");
+    console.log(testIndexOf);
+
+    const testJoin = ["k", "e", "n"].join('-');
+    console.log(testJoin);
+
+    const testSplit = "Alejandro".split('');
+    console.log(testSplit);
+
+    const testSplice = ["k", "e", "n"].splice(1,1);
+    console.log(testSplice);
+
+    const testSplice2 = ["k", "e", "n"];
+    testSplice2.splice(1,1)
+    console.log(testSplice2);
+
+    const testReduce = [1,2,3,4,5,6].reduce((acc,value)=>acc+value, 2);
+    console.log(testReduce);
+
+
+    const testEntries = {name:"Ken", key:"RPC"};
+    console.log("Stuff",Object.entries(testEntries));
+
+    const testKey = {name:"Ken", key:"RPC"};
+    console.log("Stuff2",Object.keys(testKey));
+    console.log("Stuff3",Object.values(testKey));
+
+    let testLetThisBeAVariableInCOntext = 5;
+    
+    const array1 = [1,2,3,4,5,6];
+    const array2 = [9,10,11,...array1]; //called SPREAD, not only for arrays, and used frequently in Redux
+    console.log(array2)
+
+    const per3 = {name : "a", age : 12, data1:"c", data2:"d"} //Desestructuracion
+
+    const {name, age} = per3
+
+    console.log(name, age)
+
+    const {data2:dat}= per3
+
+    console.log(dat)
+
+
+
+
+
+
+
+  }
+
 }

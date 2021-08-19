974e36cefb3a3a1288a483db603566a0a967c7e7
0WLB34R <knhrvs@gmail.com>
2021-08-19 18:22:06 -0400

    14/08/2021 nullish coalescing, optional parmas,code reduction

--- src/app/pages/admin/admin2/admin2.component.ts
+++ src/app/pages/admin/admin2/admin2.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-admin2',
-  templateUrl: './admin2.component.html',
-  styleUrls: ['./admin2.component.css']
+  selector: 'admin2',
+  template: `
+  <div>I AM ADMIN 2</div>
+  <p>{{test2 ?? test3}}</p>
+  <p>{{test2 || 'the other one'}}</p>
+  <button (click)="testParams('111111')"> test </button>`,
+  
+  styles: [` div{ background-color: green; }`]
 })
 export class Admin2Component implements OnInit {
+
+  test2 = null;
+  test3 = 'maria';
 
   constructor() { }
 
   ngOnInit() {
   }
+
+  /**
+   * @params name
+   * @params lastName
+   * return name and LastName joined
+   * 
+   */
+  public testParams(name:string, lastName?: string = 'gutierrez'): string {
+    const aux = 'juan';
+    console.log((name ?? aux) + ' '+lastName);
+    return name +' '+lastName; 
+  }
+
 
 }

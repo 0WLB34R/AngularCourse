48230e40c6d27f5753da9f106161fc1a2a6bf713
0WLB34R <knhrvs@gmail.com>
2021-08-23 15:23:15 -0400

    CLEANSLATE

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,36 +1,17 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
-import { CoreModule } from './core/core.module';
-import { AdminModule } from './pages/admin/admin.module';
-import { BuyModule } from './pages/buy/buy.module';
-import { HomeModule } from './pages/home/home.module';
-import { Test1Component } from './test1/test1.component';
-import { Test2Component } from './test2/test2.component';
-import { DirectivaDirective } from './test3/directiva.directive';
-import { Directive2Directive } from './test3/directive2.directive';
-import { Test3Component } from './test3/test3.component';
-import { Testmodule1Module } from './testmodule1/testmodule1.module';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    Test1Component,
-    Test2Component,
-    Test3Component,
-    DirectivaDirective,
-    Directive2Directive
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HomeModule,
-    AdminModule,
-    BuyModule,
-    CoreModule
-
   ],
   providers: [],
   bootstrap: [AppComponent]

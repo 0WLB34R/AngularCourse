4a56f56bf6e2d68e6f1e034232ed9129c7997296
0WLB34R <knhrvs@gmail.com>
2021-09-06 17:19:39 -0400

    quickfix

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -11,83 +11,18 @@ import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
 })
 export class LoginComponent implements OnInit {
 
-  name = new FormControl('');
+  name:string=''; 
 
 
-  formReactive: FormGroup;
 
-  constructor(private formBuilder: FormBuilder, private testService:Test1Service, private singletonService:SingletonService, private publicationService: PublicationService) { 
-    console.log(this.testService.getItems());
-
-    this.formReactive = this.formBuilder.group({
-      name: '',
-      lastName: ['', [Validators.required]],
-      date: ''
-    });
+  constructor() { 
+  
   }
+ngOnInit(): void {
 
-  ngOnInit() {
-    this.publicationService.getAll().subscribe( res => console.log(res));
-    this.formReactive.valueChanges.subscribe(res => {
-      console.log('FORM REACTOVE', res);
-    })
+}
 
-    this.name.valueChanges.subscribe(res => {
-      console.log('CHANGES', res)
-    });
-  }
 
-  setMessage(){
-    this.singletonService.setMessage("hi form login");
-  }
-
-  update() {
-
-    this.publicationService.updateById('p0002',{
-      "date": "17/82/2021",
-      "description": "update from frontend",
-      "idUser": 2,
-      "imageUrl": "http://fitebase/img.jpg",
-      "userPhotoUrl": "http://fitebase/user1.jpg"
-    } ).subscribe(
-      res => console.log(res))
-  }
-
-  create(){
-
-    this.publicationService.create({
-      "date": "17/82/2021",
-      "description": "soy nuevo",
-      "idUser": 2,
-      "imageUrl": "http://fitebase/img.jpg",
-      "userPhotoUrl": "http://fitebase/user1.jpg"
-    } ).subscribe(
-      res => console.log(res))
-
-  }
-
-  delete(){
-    this.publicationService.delete('p0002').subscribe(
-      res => console.log(res))
-  }
-
-  patch() {
-    this.publicationService.patch('p0001',{
-      "description": "update from frontend patch",
-    } ).subscribe(
-      res => console.log(res))
-  }
-
-  onSubmitTemplate(values:any){
-    console.log('VALUES: ', values)
-  }
-
-  onShow(){
-    console.log(this.name.value)
-  }
-
-  onShowAll(){
-    console.log('DDDDD', this.formReactive.value)
-  }
+ 
 
 }

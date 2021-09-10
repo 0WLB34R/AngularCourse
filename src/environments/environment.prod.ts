844013af5ada43428aa0e9e8580dc116831e8311
0WLB34R <knhrvs@gmail.com>
2021-09-09 20:37:20 -0400

    sign in with email and password

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,9 @@ export const environment = {
   production: true,
   app: {
     apiBaseUrl: "https://facebook-angular-course-default-rtdb.firebaseio.com"
+  },
+  auth: {
+    apiBaseUrl: "https://identitytoolkit.googleapis.com",
+    key: "AIzaSyD0Hh_FA6YmOWnt8AyMus94XIIgvGLBgXU"
   }
 };
